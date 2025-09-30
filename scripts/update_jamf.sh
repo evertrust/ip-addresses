b7f5697e603f405cd911a80c3b86c13d3bd60386
Antonin <agu@evertrust.fr>
2025-09-30 11:37:53 +0200

    fix(jamf): use official jamf ips JSON endpoint (#4)

--- scripts/update_jamf.sh
+++ scripts/update_jamf.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-download_url="https://learn-be.jamf.com/api/bundle/technical-articles/page/Permitting_InboundOutbound_Traffic_with_Jamf_Cloud.html"
-
 # Store the raw HTML into some variable
-raw_response=$(curl -s -H "user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 14_7_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15" "$download_url")
+raw_response=$(curl -s "https://engineering.jamf.com/tc-docs-public-ip-lists/public-ips.json")
 
-# Parse the JSON response and extract only IPs used for Azure AD SCIM provisioning
-echo "$raw_response" | jq -r '.topic_html' | htmlq 'table li' --text | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | sed 's/$/\/32/' > jamf_ips.txt
+# Parse the JSON response and extract only IPs used for Jamf outbound traffic
+echo "$raw_response" | jq -r '.public_ips[] | select(.traffic == "outbound") | .addresses[]' | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' > jamf_ips.txt
 
 if [ ! -s jamf_ips.txt ]; then
   echo "0 IP address fetched from Jamf. There might be a parsing problem."
