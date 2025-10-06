e1f3703a8e2f978ff3d1291c7ed970433389cb5a
Antonin <agu@evertrust.fr>
2025-10-06 12:07:14 +0200

    fix: follow jamf api change (#5)

--- scripts/update_jamf.sh
+++ scripts/update_jamf.sh
@@ -4,7 +4,7 @@
 raw_response=$(curl -s "https://engineering.jamf.com/tc-docs-public-ip-lists/public-ips.json")
 
 # Parse the JSON response and extract only IPs used for Jamf outbound traffic
-echo "$raw_response" | jq -r '.public_ips[] | select(.traffic == "outbound") | .addresses[]' | grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' > jamf_ips.txt
+echo "$raw_response" | jq -r '.public_ips[] | select(.traffic == "outbound") | .ip_prefixes[]' > jamf_ips.txt
 
 if [ ! -s jamf_ips.txt ]; then
   echo "0 IP address fetched from Jamf. There might be a parsing problem."
